ee72170d4205919bfc14bee5341e6dbc5693a2a2
rodleyorosa <rodleyorosa@gmail.com>
2023-02-11 19:25:37 +0100

    layout

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,31 @@
 <template>
-  <div>
-    <div>
-      <h1>test</h1>
+  <div class="h-screen bg-gray-100">
+    <Modal
+    v-if="null" />
+    <div class="py-10">
+      <div class="max-w-md m-auto">
+        <div class="flex justify-between">
+          <h1 class="font-bold text-4xl">NOTES</h1>
+          <button class="bg-neutral-800 text-gray-100 rounded-full w-10 h-10 transition ease-in-out duration-300 hover:bg-neutral-900">
+            +
+          </button>
+        </div>
+        <p class="flex items-center">
+          * Done with
+          <img class="w-4 ml-2" src="./assets/pinia.svg" alt="pinia" />
+          <span class="font-semibold text-neutral-700 text-lg">Pinia</span>
+        </p>
+      </div>
+      <div class="max-w-6xl m-auto py-10 flex flex-wrap justify-around">
+      <!-- <div v-for="(note, id) in notes" :key="id"> -->
+        <Card />
+      <!-- </div> -->
+    </div>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  
-}
+<script setup>
+import Card from './components/Card.vue'
+import Modal from './components/Modal.vue'
 </script>
---
+++ src/components/ModalDelete.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="relative z-10">
+      <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+      <div class="fixed inset-0 z-10 overflow-y-auto">
+        <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+            <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+              <div class="sm:flex sm:items-start">
+                <div class="mt-3 text-center w-full sm:mt-0 sm:text-left">
+                 <h2 class="font-bold text-2xl text-red-500">DELETE</h2>
+                 <p>Are you sure to delete this note?</p>
+                </div>
+              </div>
+            </div>
+            <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+              <button type="button"
+              class="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+              >Delete</button>
+              <button type="button"
+              class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+              >Cancel</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script setup>
+</script>
